a4439da32dada72e6f6161178992e50539d8fad4
Oleksha <deryabino@gmail.com>
2023-10-13 18:39:31 +0400

    Добавлен Класс диспетчеризации

--- src/Routes.php
+++ src/Routes.php
@@ -1,10 +1,5 @@
 <?php declare(strict_types = 1);
 
 return [
-  ['GET', '/hello-world', function () {
-    echo 'Привет мир!';
-  }],
-  ['GET', '/another-route', function () {
-    echo 'Это тоже работает';
-  }],
+  ['GET', '/', ['Example\Controllers\Homepage', 'show']],
 ];
